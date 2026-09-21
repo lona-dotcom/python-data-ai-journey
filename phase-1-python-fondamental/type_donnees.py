3e2f78ff20fb9444d7e512b020de77608cf36c19
RAZANAKOTO Lovatahiana Nomenjanahary <lonafstgi@gmail.com>
2026-09-21 16:43:18 +0300

    td

---
+++ phase-1-python-fondamental/type_donnees.py
@@ -0,0 +1,20 @@
+ville="Antananarivo"
+print(type(ville))
+age=42
+print(type(age))
+temperature=24.5
+print(type(temperature))
+est_ensoleille=True
+print(type(est_ensoleille))
+
+# On crée  4 variables de types différents
+question="Quelle est la capitale de Madagascar?"
+code_reponse =200
+score_confiance=0.87
+requete_reussie=True
+
+# On affiche chaque variable et son type
+print(question, "->", type(question))
+print(code_reponse, "->", type(code_reponse))
+print(score_confiance, "->", type(score_confiance))
+print(requete_reussie, "->", type(requete_reussie))
