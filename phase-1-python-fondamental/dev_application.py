a62988425b837bd9f2cf4d855dfeb0b9b49beb8f
RAZANAKOTO Lovatahiana Nomenjanahary <lonafstgi@gmail.com>
2026-09-24 06:57:12 +0300

    dev-application

---
+++ phase-1-python-fondamental/dev_application.py
@@ -0,0 +1,8 @@
+agent_name = "supervisor"
+agent_id = 123
+agent_strength = 0.9
+agent_active = True
+agent_log = ["history", "chat", "search"]
+agent_answer = {"title": "retour", "time": "100"}
+
+print(agent_name, agent_id, agent_strength, agent_active, agent_log, agent_answer)
