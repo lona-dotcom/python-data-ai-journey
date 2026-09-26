ab8b9aaced1677fb1a8ac18878c2e8eca8870c66
RAZANAKOTO Lovatahiana Nomenjanahary <lonafstgi@gmail.com>
2026-09-26 07:02:20 +0300

    exo1-reconnaissance

---
+++ phase-1-python-fondamental/py_exo1_reconnaissace.py
@@ -0,0 +1,12 @@
+temperature = 8
+if temperature >= 25:
+    ressenti = "Il fait chaud"
+elif temperature >= 15:
+    ressenti = "Il fait doux"
+elif temperature >= 5:
+    ressenti = "Il fait frais"
+else:
+    ressenti = "Il fait froid"
+print(ressenti)
+
+# affichage du ressenti: "Il fait frais"
